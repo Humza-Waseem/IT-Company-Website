ad7cd7af8f01566c6b6f655177c998711416b867
HamXa <hamza.wasym99@gmail.com>
2024-07-19 22:56:42 +0500

    newStylePart1

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,30 +1,30 @@
-/////////////////   navbar ///////////////////////
+// /////////////////   navbar ///////////////////////
 
-window.addEventListener('scroll', function() {
-    const navbar = document.querySelector('.navBar');
-    if (window.scrollY > 0) {
-        if (window.scrollY > 0) {
-            navbar.classList.add('navbar-scrolled');
-            console.log(navbar.classList); // Log the class list to the console
-        }
-        // navbar.classList.add('navbar-scrolled');
-    } else {
-        navbar.classList.remove('navbar-scrolled');
-    }
-});
+// window.addEventListener('scroll', function() {
+//     const navbar = document.querySelector('.navBar');
+//     if (window.scrollY > 0) {
+//         if (window.scrollY > 0) {
+//             navbar.classList.add('navbar-scrolled');
+//             console.log(navbar.classList); // Log the class list to the console
+//         }
+//         // navbar.classList.add('navbar-scrolled');
+//     } else {
+//         navbar.classList.remove('navbar-scrolled');
+//     }
+// });
 
-window.addEventListener('scroll', function() {
-    const navbar = document.querySelector('.navBar');
-    if (window.scrollY > 0) {
-        if (window.scrollY > 0) {
-            navbar.classList.add('navbar-scrolled-logo');
-            console.log(navbar.classList); // Log the class list to the console
-        }
-        // navbar.classList.add('navbar-scrolled');
-    } else {
-        navbar.classList.remove('navbar-scrolled-logo');
-    }
-});
+// window.addEventListener('scroll', function() {
+//     const navbar = document.querySelector('.navBar');
+//     if (window.scrollY > 0) {
+//         if (window.scrollY > 0) {
+//             navbar.classList.add('navbar-scrolled-logo');
+//             console.log(navbar.classList); // Log the class list to the console
+//         }
+//         // navbar.classList.add('navbar-scrolled');
+//     } else {
+//         navbar.classList.remove('navbar-scrolled-logo');
+//     }
+// });
 
 
 
